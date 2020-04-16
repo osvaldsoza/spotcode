62b7873f6bda837c09ec3fd620eddca5a93989f0
osvaldsoza <osvald.soza@gmail.com>
2020-04-16 00:16:58 -0300

    inclusao de endpoints

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  has_many :albums
-  validates :name, presence: true
-  has_one_attached :image
+    has_many :albums
+    validates :name, presence: true
+    has_one_attached :image
 end
